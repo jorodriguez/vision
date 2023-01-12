8b1af70591980fde672b571b866425c42f5fe029
= <=>
2023-01-11 21:28:49 -0600

    cambios ya se puede ller los datos

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = require('./routes/app');
 const vision = require('@google-cloud/vision');
 
-const https = require("https");
+const fs = require("fs");
+const { url } = require('inspector');
 
 
 //Subir imagen
-app.get('/test', async(req, res) => {
+app.get('/test/:imagen', async(req, res) => {
     // Imports the Google Cloud client library
 
 
@@ -18,12 +19,77 @@ app.get('/test', async(req, res) => {
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
     */
+    const imagen = req.params.imagen;
 
-    const [result] = await client.textDetection('/home/jorodriguez/Descargas/credencia1.jpeg');
-    const detections = result.textAnnotations;
-    console.log('Text:');
-    detections.forEach(text => console.log(text));
-    res.status(200).json(detections);
+    if(!imagen){
+
+        res.status(200).send("selecciona una foto");
+    }
+
+    const urlImagen = `/home/joel/Documentos/imagenes_prueba/${imagen}`;
+
+    const existeImagen = fs.existsSync(urlImagen);
+
+    if(!existeImagen){
+        res.status(200).send("No existe la imagen");
+    }    
+    const [result] = await client.textDetection(urlImagen);
+    const detections = result.textAnnotations;    
+    //detections.forEach(text => console.log(text));
+
+    let  imgBase64 = fs.readFileSync(urlImagen,'base64');
+    
+    console.log(detections);
+    console.log("-------------");
+
+    console.log(detections[0].description);
+
+    const texto = JSON.stringify(detections[0].description);
+
+    const arrayText = texto.split(/\\n/g);
+
+
+    const arrayLabels = ['NOMBRE','DOMICILIO','INSTITUTO NACIONAL ELECTORAL','COL.','CLAVE DE ELECTOR','CURP','MUNICIPIO','SECCIÓN','EMISIÓN','VIGENCIA','FECHA NACIMIENTO','SEXO','LOCALIDAD'];
+
+    //${texto.replace(/\\n/g,'<br/>')}
+
+    let html;
+    
+    arrayText.forEach(text => {                                
+        html += (arrayLabels.includes(text) ? '<strong>'+text+'</strong>':text)+'<br/>';
+    });
+
+    console.log(imgBase64);
+    
+    res.set('Content-Type', 'text/html');
+    
+    res.status(200).send(`
+
+                        <table border="1">
+                            <tr colspan="2"><td>${urlImagen}</td></tr>
+                            <tr>
+                            <td style="width:50%"><img src="data:image/png;base64, ${imgBase64}" style="width: 80%;"/> </td>
+                            <td> <p> ${html} </p></td>
+                            </tr>
+                            </table>
+                            
+                            `);
+    
+
+    /*const [result] = await client.faceDetection('/home/joel/Imágenes/credencialprueba.png');
+    const faces = result.faceAnnotations;
+    console.log('Faces:');
+    faces.forEach((face, i) => {
+    console.log(`  Face #${i + 1}:`);
+    console.log(`    Joy: ${face.joyLikelihood}`);
+    console.log(`    Anger: ${face.angerLikelihood}`);
+    console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+    console.log(`    Surprise: ${face.surpriseLikelihood}`);
+      });
+      res.status(200).json(faces);
+      */
+
+    
 });
 
 
